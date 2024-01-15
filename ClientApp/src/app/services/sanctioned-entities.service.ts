cb56677f5b1e82226121cfa6e00d90604b3ab6b1
Rob Gordula <rob@gordula.info>
2024-01-15 01:46:55 +0000

    Added methods for getting and updating the current count from the web api

--- ClientApp/src/app/services/sanctioned-entities.service.ts
+++ ClientApp/src/app/services/sanctioned-entities.service.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { SanctionedEntity } from '../models/sanctioned-entity';
 import { Observable } from 'rxjs';
 
@@ -9,7 +9,12 @@ import { Observable } from 'rxjs';
 export class SanctionedEntitiesService {
 
   private readonly apiUrl: string;
-  private readonly path = 'sanctioned-entities';
+  private readonly path = 'sanctioned-entities'
+
+  private readonly httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
 
   constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     this.apiUrl = baseUrl + 'api/';
@@ -18,5 +23,15 @@ export class SanctionedEntitiesService {
   public getSanctionedEntities(): Observable<SanctionedEntity[]> {
     const url = this.apiUrl + this.path;
     return this.http.get<SanctionedEntity[]>(url);
+  }
+
+  public getSanctionedEntitiesCount(): Observable<number> {
+    const url = `${this.apiUrl}${this.path}/count`;
+    return this.http.get<number>(url);
+  }
+
+  public updateSanctionedEntitiesCount(count: number): Observable<number> {
+    const url = `${this.apiUrl}${this.path}/new-count/${count}`;
+    return this.http.post<number>(url, count, this.httpOptions);
   }
 }
