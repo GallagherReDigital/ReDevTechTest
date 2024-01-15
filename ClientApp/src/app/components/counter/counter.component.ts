8c48c7bdc474b306e758afc1259e08ebaea883b9
Rob Gordula <rob@gordula.info>
2024-01-15 01:40:32 +0000

    Extracted the current count view into its own component

--- ClientApp/src/app/components/counter/counter.component.ts
+++ ClientApp/src/app/components/counter/counter.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 
+import { SanctionedEntitiesService } from '../../services/sanctioned-entities.service';
+
 @Component({
   selector: 'app-counter-component',
   templateUrl: './counter.component.html'
 })
 export class CounterComponent {
-  public currentCount = 0;
+  public currentCount: number = 0;
+
+  constructor(private entitiesService: SanctionedEntitiesService) {
+    this.entitiesService.getSanctionedEntitiesCount().subscribe(count => this.currentCount = count);
+  }
 
   public incrementCounter() {
     this.currentCount++;
+    this.entitiesService.updateSanctionedEntitiesCount(this.currentCount).subscribe();
   }
 }
