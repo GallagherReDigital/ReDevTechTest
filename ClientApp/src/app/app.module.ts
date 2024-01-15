9152675670d45cfd4f517a877ca5d489a9b28a06
Rob Gordula <rob@gordula.info>
2024-01-15 01:47:56 +0000

    Added reference to all the new components

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -10,6 +10,9 @@ import { HomeComponent } from './components/home/home.component';
 import { CounterComponent } from './components/counter/counter.component';
 import { SanctionedEntitiesComponent } from './components/sanctioned-entities/sanctioned-entities.component';
 import { JumbotronCounterComponent } from './components/jumbotron-counter/jumbotron-counter.component';
+import { CurrentCountComponent } from './components/current-count/current-count.component';
+import { SanctionedEntityModal } from './components/sanctioned-entity-modal/sanctioned-entity-modal.component';
+import { SanctionedEntityEditor } from './components/sanctioned-entity-editor/sanctioned-entity-editor.component';
 
 
 @NgModule({
@@ -19,12 +22,16 @@ import { JumbotronCounterComponent } from './components/jumbotron-counter/jumbot
     HomeComponent,
     CounterComponent,
     SanctionedEntitiesComponent,
-    JumbotronCounterComponent
+    JumbotronCounterComponent,
+    CurrentCountComponent,
+    SanctionedEntityModal,
+    SanctionedEntityEditor
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
