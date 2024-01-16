02ba12c53d51e8cce6f9790d623b2267d46fe1a3
Rob Gordula <rob@gordula.info>
2024-01-16 16:16:17 +0000

    Fixed failing tests

--- ClientApp/src/app/components/current-count/current-count.component.spec.ts
+++ ClientApp/src/app/components/current-count/current-count.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { CurrentCountComponent } from './current-count.component';
+import { SanctionedEntitiesService } from '../../services/sanctioned-entities.service';
 
 describe('CurrentCountComponent', () => {
   let component: CurrentCountComponent;
@@ -8,7 +10,16 @@ describe('CurrentCountComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CurrentCountComponent ]
+      declarations: [
+        CurrentCountComponent
+      ],
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        SanctionedEntitiesService,
+        { provide: 'BASE_URL', useValue: '/' }
+      ]
     })
     .compileComponents();
 
@@ -19,5 +30,15 @@ describe('CurrentCountComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should start with count 0, then increments by 1 when clicked', () => {
+    const countElement = fixture.nativeElement.querySelector('strong');
+    expect(countElement.textContent).toEqual('0');
+
+    const incrementButton = fixture.nativeElement.querySelector('button');
+    incrementButton.click();
+    fixture.detectChanges();
+    expect(countElement.textContent).toEqual('1');
   });
 });
