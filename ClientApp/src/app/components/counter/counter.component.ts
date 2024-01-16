a3df04afb28a3b09a066d9bbd5e05a189ab3a6d7
Rob Gordula <rob@gordula.info>
2024-01-16 11:27:03 +0000

    Moved the increment button and all functionality into the current count component

--- ClientApp/src/app/components/counter/counter.component.ts
+++ ClientApp/src/app/components/counter/counter.component.ts
@@ -1,20 +1,8 @@
 import { Component } from '@angular/core';
 
-import { SanctionedEntitiesService } from '../../services/sanctioned-entities.service';
-
 @Component({
   selector: 'app-counter-component',
   templateUrl: './counter.component.html'
 })
 export class CounterComponent {
-  public currentCount: number = 0;
-
-  constructor(private entitiesService: SanctionedEntitiesService) {
-    this.entitiesService.getSanctionedEntitiesCount().subscribe(count => this.currentCount = count);
-  }
-
-  public incrementCounter() {
-    this.currentCount++;
-    this.entitiesService.updateSanctionedEntitiesCount(this.currentCount).subscribe();
-  }
 }
--- ClientApp/src/app/components/current-count/current-count.component.ts
+++ ClientApp/src/app/components/current-count/current-count.component.ts
@@ -1,9 +1,22 @@
 import { Component, Input } from '@angular/core';
 
+import { SanctionedEntitiesService } from '../../services/sanctioned-entities.service';
+
 @Component({
   selector: 'counter-current-count',
   templateUrl: './current-count.component.html'
 })
 export class CurrentCountComponent {
-  @Input() count = -1;
+  public currentCount: number = 0;
+
+  constructor(private entitiesService: SanctionedEntitiesService) {
+    this.entitiesService.getSanctionedEntitiesCount()
+      .subscribe(count => this.currentCount = count);
+  }
+
+  public incrementCounter() {
+    this.currentCount++;
+    this.entitiesService.updateSanctionedEntitiesCount(this.currentCount)
+      .subscribe();
+  }
 }
